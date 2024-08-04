d9dff4171847a1da2e2a4bd077cd67ff30586737
Mikkel ALMONTE--RINGAUD <vexitofficial@gmail.com>
2024-08-04 15:57:03 +0200

    fix(npm): remove old wasm build entry

--- npm/tsup.config.ts
+++ npm/tsup.config.ts
@@ -1,27 +1,13 @@
-import { defineConfig } from 'tsup';
+import { defineConfig } from "tsup";
 
-export default defineConfig([
-  {
-    entry: ['src/index.ts'],
+export default defineConfig({
+  entry: ["src/index.ts"],
 
-    outDir: 'dist',
-    format: ['cjs', 'esm'],
-    
-    sourcemap: true,
-    minify: "terser",
-    clean: true,
-    dts: true
-  },
-  {
-    entry: ["src/build.ts"],
+  outDir: "dist",
+  format: ["cjs", "esm"],
 
-    outDir: 'dist',
-    format: ['esm'],
-    
-    sourcemap: true,
-    minify: "terser",
-    clean: true,
-    // as a dependency.
-    external: ["terser"]
-  }
-])
+  sourcemap: true,
+  minify: "terser",
+  clean: true,
+  dts: true,
+});

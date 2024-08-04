0a17de86da200f80ac5e025408ac116d4581b2e3
Mikkel ALMONTE--RINGAUD <vexitofficial@gmail.com>
2024-08-04 19:14:20 +0200

    chore: init

---
+++ README.md
@@ -0,0 +1,7 @@
+# LiterateInk: `utilities`
+
+A collection of utilities for our libraries to prevent us from writing the same code again and again in multiple repositories that finally gets out of sync.
+
+## Documentation
+
+You can find what those utilities do at <https://docs.literate.ink/utilities>.
---
+++ src/index.ts
@@ -0,0 +1,67 @@
+import { splitCookiesString } from "set-cookie-parser";
+
+interface HeadersLike {
+  get (key: string): string | null
+};
+
+/**
+ * Extracts cookies from the "set-cookie" header of a response.
+ */
+export const getCookiesFromResponse = (response: Response): string[] => {
+  const setCookieHeader = getHeaderFromResponse(response, "set-cookie");
+  if (setCookieHeader === null) return [];
+
+  return splitCookiesString(setCookieHeader)
+    .map((cookie) => cookie.split(";")[0]);
+};
+
+/**
+ * Extracts a header from a response.
+ */
+export const getHeaderFromResponse = (response: Response, item: string): string | null => {
+  const headers = response.headers;
+
+  return typeof headers.get === "function"
+    ? headers.get(item)
+    : (headers as Record<string, string>)[item];
+};
+
+export interface Request {
+  url: string
+  method: string
+  /** Body of the request of type given in the "Content-Type" header. */
+  content: string | undefined
+  /** Headers that should be appended to the request. */
+  headers: Record<string, string> | Headers
+  redirect: "follow" | "manual"
+}
+
+export interface Response {
+  status: number
+  content: string
+  headers: Record<string, string> | Headers | HeadersLike
+}
+
+export type Fetcher = (req: Request) => Promise<Response>;
+
+/**
+ * Simple and default fetcher using `fetch` if none was given
+ * in the authentication function.
+ * 
+ * Should work out-of-the-box on Node.js>=18, Deno, Bun, React Native
+ * and probably more environments.
+ */
+export const defaultFetcher = async (req: Request): Promise<Response> => {
+  const response = await fetch(req.url, {
+    redirect: req.redirect,
+    headers: req.headers,
+    method: req.method,
+    body: req.content
+  });
+
+  return {
+    status: response.status,
+    content: await response.text(),
+    headers: response.headers
+  };
+}

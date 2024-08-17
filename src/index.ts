3fd7c14fa9ed3bb89c24f9bd6c74b18014077f4c
Mikkel ALMONTE--RINGAUD <vexitofficial@gmail.com>
2024-08-17 17:36:36 +0200

    feat: add `findValueBetween()`

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,11 @@
 import { splitCookiesString } from "set-cookie-parser";
 
+export const findValueBetween = (plain: string, start: string, end: string): string => {
+  const startIndex = plain.indexOf(start) + start.length;
+  const endIndex = plain.indexOf(end, startIndex);
+  return plain.slice(startIndex, endIndex);
+};
+
 interface HeadersLike {
   get (key: string): string | null
 };
